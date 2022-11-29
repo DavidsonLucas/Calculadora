54ed1b3eb2dcb158939360cfb7f277adb5293bc8
DavidsonLucas <davidson.lucas.oiveira@gamil.com>
2022-11-29 18:04:34 -0300

    Calculadora 29/11/2022

---
+++ app.js
@@ -0,0 +1,23 @@
+
+    function adicionarCaracter(caracter) {
+        const valorDisplay = document.querySelector(".display").value
+
+
+        document.querySelector(".display").value = valorDisplay + caracter
+    }
+
+    function limpaTela() {
+        document.querySelector(".display").value = ""
+    }
+
+    function calcular() {
+        const valorDisplay = document.querySelector(".display").value
+        document.querySelector(".display").value = eval(valorDisplay)
+    }
+
+    function inverterNumero() {
+        const valorDisplay = document.querySelector(".display").value
+        document.querySelector(".display").value = valorDisplay * -1
+    } 
+
+
