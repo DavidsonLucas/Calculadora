b0152e4f4ee28a3f31eedc4b536786819e617b6d
DAVIDSON LUCAS SALES DE OLIVEIRA <106599546+DavidsonLucas@users.noreply.github.com>
2022-12-03 20:22:14 -0300

    munanças no js e no html e keydown

--- app.js
+++ app.js
@@ -1,23 +1,121 @@
+function adicionarCarater(caracter) {
+    const valorDisplay = document.querySelector(".display").value
+    
+    document.querySelector(".display").value = valorDisplay + caracter
+}
 
-    function adicionarCaracter(caracter) {
+function limparTela() {
+    document.querySelector(".display").value = ""
+}
+
+function calcular() {
+    const valorDisplay = document.querySelector(".display").value
+    document.querySelector(".display").value = eval(valorDisplay)
+}
+
+function inverterNumero() {
+    const valorDisplay = document.querySelector(".display").value
+    document.querySelector(".display").value = valorDisplay * -1
+
+} 
+
+document.addEventListener("keydown",function(event){
+    if(event.key=== "0"){
+    const valorDisplay = document.querySelector(".display").value
+
+    document.querySelector(".display").value = valorDisplay + 0
+        
+    }
+    if(event.key=== "1"){
+    const valorDisplay = document.querySelector(".display").value
+
+    document.querySelector(".display").value =  valorDisplay + 1
+    }
+    if(event.key=== "2"){
+    const valorDisplay = document.querySelector(".display").value
+
+    document.querySelector(".display").value =  valorDisplay + 2
+        
+    }
+    if(event.key=== "3"){
+    const valorDisplay = document.querySelector(".display").value
+        
+    document.querySelector(".display").value =  valorDisplay + 3 
+        
+    }
+    if(event.key=== "4"){
+    const valorDisplay = document.querySelector(".display").value
+
+    document.querySelector(".display").value =  valorDisplay + 4
+         
+    }
+    if(event.key=== "5"){
+    const valorDisplay = document.querySelector(".display").value
+
+    document.querySelector(".display").value =  valorDisplay + 5
+        
+    }
+    if(event.key=== "6"){
+    const valorDisplay = document.querySelector(".display").value
+
+    document.querySelector(".display").value =  valorDisplay + 6
+        
+    }
+    if(event.key=== "7"){
+    const valorDisplay = document.querySelector(".display").value
+
+    document.querySelector(".display").value =  valorDisplay + 7
+        
+    }
+    if(event.key=== "8"){
+    const valorDisplay = document.querySelector(".display").value
+
+    document.querySelector(".display").value =  valorDisplay + 8
+        
+    }
+    if(event.key === "9"){
         const valorDisplay = document.querySelector(".display").value
 
-
-        document.querySelector(".display").value = valorDisplay + caracter
+        document.querySelector(".display").value = valorDisplay + 9
     }
-
-    function limpaTela() {
-        document.querySelector(".display").value = ""
+    if(event.key=== "Backspace"){
+         document.querySelector(".display").value = ""
     }
+     if(event.key=== "="){
+    const valorDisplay = document.querySelector(".display").value
 
-    function calcular() {
+    document.querySelector(".display").value = eval(valorDisplay)
+    }
+    if(event.key === "+"){
         const valorDisplay = document.querySelector(".display").value
-        document.querySelector(".display").value = eval(valorDisplay)
-    }
 
-    function inverterNumero() {
+        document.querySelector(".display").value = valorDisplay + "+"
+    }
+    if(event.key === "-"){
         const valorDisplay = document.querySelector(".display").value
-        document.querySelector(".display").value = valorDisplay * -1
-    } 
+
+        document.querySelector(".display").value = valorDisplay + "-"
+    }
+    if(event.key === "/"){
+        const valorDisplay = document.querySelector(".display").value
+
+        document.querySelector(".display").value = valorDisplay + "/"
+    }
+    if(event.key === "*"){
+        const valorDisplay = document.querySelector(".display").value
+
+        document.querySelector(".display").value = valorDisplay + "*"
+    }
+    if(event.key === "Enter"){
+    const valorDisplay = document.querySelector(".display").value
+    
+    document.querySelector(".display").value = eval(valorDisplay)
+    }
+    if(event.key === "."){
+    const valorDisplay = document.querySelector(".display").value
+    
+    document.querySelector(".display").value = valorDisplay + "."
+    };
+})
 
 
